4f58b7cd98301cf2a0cd082a78e333c463b7cc5c
James Alday <arclyte@gmail.com>
2018-10-05 08:43:36 -0400

    Data processing script updated with models, debug logging, updated readme, added upload folder to repo

--- README.md
+++ README.md
@@ -2,13 +2,25 @@
 Data wrangling test for The Atlantic
 
 ** Files **
-- sample_data.txt - Generated using Mockaroo. Source: https://www.mockaroo.com (Schema: https://www.mockaroo.com/fa830660)
+- atlantic_data.txt - Test TSV upload data generated using Mockaroo. Source: https://www.mockaroo.com (Schema: https://www.mockaroo.com/fa830660)
 - droopy - Droopy upload server (customized to add call to process_data script). Source: https://github.com/stackp/Droopy
 - process_data.py - Script to process uploaded TSV files
+- models/* - SQLAlchemy models, base.py contains the DSN url
+- README.md - this file
+- requirements.txt - pip requirements file
+- schema.sql - mysqldump of tables required for test (contains create database statement)
+- upload/ - files processed by droopy are dropped here
+
+** Assumptions **
+- Python3 (scripts tested on version 3.6.5)
+- MySQL on localhost with no password. (Tested on version 8.0.12 Homebrew (OSX))
 
 
-*To Run:*
-mkdir ./upload
-./droopy -d ./upload
+**To Run:**
+1. Install requirements: `pip3 install -r requirements.txt`
+2. Import DB schema: `mysql -h localhost -u root < schema.sql`
+3. Run modified droopy server: `./droopy -d ./upload`
+4. Visit http://localhost:8000/ and upload a file.
+5. DB tables contain migrated data.
 
-Visit http://localhost:8000/ and upload a file.
+Note: process_data.py also contains two lines at the end of the file (marked DEBUG) that allow running it directly from the command line for testing.
--- process_data.py
+++ process_data.py
@@ -1,27 +1,88 @@
-from pprint import pprint
-
+import os, sys  # DEBUG
 import csv
+import logging
+import dateutil.parser
+
+from sqlalchemy import exc
+
+from models.base import db
+from models.Customer import CustomerModel
+from models.Product import ProductModel
+from models.Purchase import PurchaseModel
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+logger.addHandler(logging.StreamHandler(sys.stdout))  # DEBUG
 
 
 class process_data():
     def import_tsv(self, filepath):
-        pprint('Filepath: ' + filepath)
+        # logger.debug('Filepath: ' + filepath)  # DEBUG
 
         line_count = 0
 
-        with open(filepath, 'rb') as csvfile:
-            # pprint(csvfile.read(1024))
+        with open(filepath, newline='') as csvfile:
             dialect = csv.Sniffer().sniff(csvfile.read(1024))
             csvfile.seek(0)
 
             reader = csv.DictReader(csvfile, dialect=dialect)
 
             for row in reader:
-                if line_count > 10:
-                    break
-
-                pprint(row)
+                # DEBUG
+                # if line_count > 10:
+                #     break
 
                 line_count += 1
 
-        pprint('Processed {} records'.format(line_count))
+                date_parsed = dateutil.parser.parse(row['date']).strftime("%Y-%m-%d %H:%M:%S")
+
+                # Customer
+                customer = self.getRecord(CustomerModel, {'id': row['customer_id']})
+                customer.id = row['customer_id']
+                customer.first_name = row['first_name']
+                customer.last_name = row['last_name']
+                customer.address = row['address']
+                customer.state = row['state']
+                customer.postal_code = row['postal_code']
+
+                db.session.add(customer)
+
+                # Product
+                product = self.getRecord(ProductModel, {'id': row['product_id']})
+                product.id = row['product_id']
+                product.name = row['product_name'].encode("utf-8")
+
+                db.session.add(product)
+
+                # Purchase
+                purchase = self.getRecord(PurchaseModel, {
+                    'customer_id': customer.id,
+                    'product_id': row['product_id'],
+                    'date': date_parsed})
+                purchase.customer_id = row['customer_id']
+                purchase.status = row['order_status']
+                purchase.product_id = row['product_id']
+                purchase.amount = row['purchase_amount'].replace('$', '')
+                purchase.date = date_parsed
+
+                db.session.add(purchase)
+
+                db.session.commit()
+
+        # DEBUG
+        logger.info('Processed {} records'.format(line_count))
+
+    def getRecord(self, model, filters):
+        try:
+            result = db.session.query(model).filter_by(**filters).one()
+            # logger.info('Updating ' + model.__name__ + ' record.')
+        except exc.SQLAlchemyError:
+            result = model()
+            # logger.info('New ' + model.__name__ + ' record found.')
+
+        return result
+
+
+# DEBUG - uncomment this to run the
+# filepath = os.path.abspath('./atlantic_data.txt')
+# process_data().import_tsv(filepath)
